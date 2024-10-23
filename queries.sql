7a2f3adf790647f36daab874759a6629ed7781da
JrSchoolAccount <144328779+JrSchoolAccount@users.noreply.github.com>
2024-10-23 07:25:11 +0200

    Uppgift 3 Replace whitespaces

--- queries.sql
+++ queries.sql
@@ -11,3 +11,13 @@ WHERE outcome = 'Successful';
 ALTER TABLE successful_mission
 MODIFY mission_id SMALLINT NOT NULL AUTO_INCREMENT,
 ADD PRIMARY KEY (mission_id);
+--
+-- Uppgift 3
+--
+UPDATE successful_mission
+SET operator = REPLACE(operator, ' ', '')
+WHERE operator LIKE '% %';
+UPDATE moon_mission
+SET operator = REPLACE(operator, ' ', '')
+WHERE operator LIKE  '% %';
+SELECT * FROM moon_mission;
