08d1b856b728470581f145df4072261658041111
JrSchoolAccount <144328779+JrSchoolAccount@users.noreply.github.com>
2024-10-23 06:56:29 +0200

    Uppgift 2 primary key auto increment

--- queries.sql
+++ queries.sql
@@ -6,3 +6,8 @@ SELECT *
 FROM moon_mission
 WHERE outcome = 'Successful';
 --
+-- Uppgift 2
+--
+ALTER TABLE successful_mission
+MODIFY mission_id SMALLINT NOT NULL AUTO_INCREMENT,
+ADD PRIMARY KEY (mission_id);
