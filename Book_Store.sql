05ed8c45ee02af84049a4ab8616d54f3744f0ae0
JrSchoolAccount <144328779+JrSchoolAccount@users.noreply.github.com>
2024-10-24 14:57:54 +0200

    created table author with key id

--- Book_Store.sql
+++ Book_Store.sql
@@ -4,4 +4,12 @@
 --
 DROP DATABASE IF EXISTS bookstore;
 CREATE DATABASE bookstore;
-USE bookstore
+USE bookstore;
+
+CREATE TABLE author
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name  VARCHAR(100) NOT NULL,
+    birth_date DATE
+);
