8e5c3120c86d29346ad1802c10f44d770d10a9da
JrSchoolAccount <144328779+JrSchoolAccount@users.noreply.github.com>
2024-10-28 09:42:56 +0100

    Added bookstore table

--- BookStore.sql
+++ BookStore.sql
@@ -14,16 +14,25 @@ CREATE TABLE author
     birth_date DATE
 );
 
-CREATE TABLE language (
-                          id INT AUTO_INCREMENT PRIMARY KEY,
-                          lang_name VARCHAR(50) UNIQUE NOT NULL
+CREATE TABLE language
+(
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    lang_name VARCHAR(50) UNIQUE NOT NULL
 );
 
-CREATE TABLE book (
-                      isbn CHAR(13) PRIMARY KEY,
-                      title VARCHAR(255) NOT NULL,
-                      language_id INT,
-                      price DECIMAL(10, 2) NOT NULL,
-                      publication_date DATE,
-                      FOREIGN KEY (language_id) REFERENCES language(id)
+CREATE TABLE book
+(
+    isbn             CHAR(13) PRIMARY KEY,
+    title            VARCHAR(255)   NOT NULL,
+    language_id      INT,
+    price            DECIMAL(10, 2) NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (language_id) REFERENCES language (id)
+);
+
+CREATE TABLE bookstore
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    store_name VARCHAR(255) NOT NULL,
+    city       VARCHAR(100) NOT NULL
 );
